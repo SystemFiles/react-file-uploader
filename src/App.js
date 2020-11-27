291970a1aaefaecb0092b8cd80df2062c0b5ba9f
Ben Sykes <ben@sykesdev.ca>
2020-11-26 23:30:23 -0500

    Made some minor UI improvements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid } from '@material-ui/core'
 import Upload from './components/Upload'
 
@@ -6,14 +6,31 @@ import Upload from './components/Upload'
 import './App.css'
 
 export default function App() {
+
+  const [ state, setState ] = useState({
+    uploading: false,
+    uploadSuccess: false
+  })
+
+  const uploadFilesToServer = (files) => {
+    // TODO: implement
+    setState({ uploading: true })
+    setTimeout(() => {
+      setState({ uploading: false, uploadSuccess: true })
+      setTimeout(() => {
+        setState({ uploadSuccess: false })
+      }, 3000)
+    }, 2000)
+  }
+
   return (
     <div className="container">
       <Grid container spacing={0} justify="center" alignItems="center" alignContent="center" >
         <Grid className="titleContainer" item xs={10}>
-          <h2>File Uploader</h2>
+          <h2>Simple File Share</h2>
         </Grid>
         <Grid className="uploadContainer" item xs={10}>
-          <Upload />
+          <Upload onUpload={uploadFilesToServer} success={state.uploadSuccess} isUploading={state.uploading} />
         </Grid>
       </Grid>
     </div>
--- src/components/Upload.js
+++ src/components/Upload.js
@@ -2,24 +2,61 @@ import React, { useCallback, useRef, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUpload } from '@fortawesome/free-solid-svg-icons'
 import { useDropzone } from 'react-dropzone'
-import { Grid } from '@material-ui/core'
+import { Grid, CircularProgress } from '@material-ui/core'
+import CheckIcon from '@material-ui/icons/Check';
+import { grey } from '@material-ui/core/colors';
 import UploadListItem from './UploadListItem'
 import { v4 as uuid } from 'uuid'
 
 // Styles
 import './Upload.css'
 
-function Upload() {
+function Upload(props) {
 
     // Component state
-    const [state, setState] = useState({
+    const [ state, setState ] = useState({
         selectedFiles: []
     })
 
+    // Saves selected files to state for upload
+    const saveFiles = (files) => {
+        if (state.selectedFiles.length <= 6 
+            && state.selectedFiles.length + files.length <=6 ) {
+            setState((st) => ({
+                selectedFiles: [...st.selectedFiles, ...files]
+            }))
+        }
+    }
+
+    // Deletes a selected file from the list
+    const deleteSelectedFile = (fileName) => {
+        state.selectedFiles.filter(file => file.name.includes(fileName)).map((filteredFile) => {
+            let arr = state.selectedFiles
+            let index = arr.indexOf(filteredFile)
+            if (index !== -1) {
+                arr.splice(index, 1);
+                setState({
+                    selectedFiles: arr
+                })
+            }
+        })
+    }
+
+    // Passes data up to App component for upload
+    const handleUploadFiles = (e) => {
+        // Use prop to upload
+        if (state.selectedFiles.length === 0) {
+            console.warn('No files selected for upload...')
+            alert('Must select at least one file to upload!')
+        } else {
+            props.onUpload(state.selectedFiles)
+        }
+    }
+
     // Handle drag and drop of files on upload area
     const onDrop = useCallback(files => {
-        SaveFiles(files)
-    }, [])
+        saveFiles(files)
+    }, [saveFiles])
     const {getRootProps, getInputProps} = useDropzone({onDrop})
 
     // Set ref to access file input
@@ -32,66 +69,58 @@ function Upload() {
 
     // Handles what happens after user is done with file select dialog
     const handleBrowseCompleted = (e) => {
-        SaveFiles(e.target.files)
-    }
-
-    // Saves selected files to state for upload
-    const SaveFiles = (files) => {
-        if (state.selectedFiles.length <= 6 
-            && state.selectedFiles.length + files.length <=6 ) {
-            setState((st) => ({
-                selectedFiles: [...st.selectedFiles, ...files]
-            }))
-        }
-    }
-
-    const deleteSelectedFile = (fileName) => {
-        state.selectedFiles.filter(file => file.name.includes(fileName)).map((filteredFile => {
-            let arr = state.selectedFiles
-            let index = arr.indexOf(filteredFile)
-            if (index !== -1) {
-                arr.splice(index, 1);
-                setState({
-                    selectedFiles: arr
-                })
-            }
-        }))
+        saveFiles(e.target.files)
     }
 
     return (
         <Grid item container justify="flex-start" alignContent="flex-start" className="Upload-Component">
-        <Grid item xs={12} sm={7} className="Upload">
-        <div className="Upload">
-            <div className='Upload-Container' {...getRootProps()}>
-                <input hidden={true} {...getInputProps()} />
-                <input type="file" id="files" ref={fileUploaderRef} 
-                        style={{display: "none"}} 
-                        onClick={(e) => {e.stopPropagation()}} 
-                        onChange={handleBrowseCompleted} multiple />
-                <div className="Upload-Icon">
-                    <FontAwesomeIcon className="svgIcon" icon={faUpload} />
+            <Grid item xs={12} sm={7} className="Upload">
+            <div className="Upload">
+                <div className='Upload-Container' {...getRootProps()}>
+                    <input hidden={true} {...getInputProps()} />
+                    <input type="file" id="files" ref={fileUploaderRef} 
+                            style={{display: "none"}} 
+                            onClick={(e) => {e.stopPropagation()}} 
+                            onChange={handleBrowseCompleted} multiple />
+                    <div className="Upload-Icon">
+                        <FontAwesomeIcon className="svgIcon" icon={faUpload} />
+                    </div>
+                    <div className="Upload-Label">
+                        <h2>
+                            Drag and Drop file
+                        </h2>
+                    </div>
                 </div>
-                <div className="Upload-Label">
-                    <h2>
-                        Drag and Drop file
-                    </h2>
+                <h2>Or</h2>
+                <div className="Upload-Browse">
+                    <a className="Upload-Button" onClick={handleBrowseClick} href="#">
+                        Browse
+                    </a>
                 </div>
             </div>
-            <h2>Or</h2>
-            <div className="Upload-Browse">
-                <a className="Upload-Button" onClick={handleBrowseClick} href="#">
-                    Browse
-                </a>
-            </div>
-        </div>
-        </Grid>
-        <Grid item xs={12} sm={5} className="Upload-List">
-            <div className="Upload-List-Container">
-                { state.selectedFiles.map((file) => (
-                    <UploadListItem key={uuid()} file={file} onClick={deleteSelectedFile} /> 
-                ))}
-            </div>
-        </Grid>
+            </Grid>
+            <Grid item xs={12} sm={5} className="Upload-List">
+                    <div className="Upload-List-Container">
+                        { state.selectedFiles.map((file) => (
+                            <UploadListItem key={uuid()} file={file} onClick={deleteSelectedFile} /> 
+                        ))}
+                    </div>
+            </Grid>
+            <Grid item xs={12} className="Upload-Submit-Container">
+                <button type="submit" disabled={props.isUploading || props.success} onClick={handleUploadFiles}>
+                    {
+                        props.isUploading ?
+                        <CircularProgress size={24} className="fabProgress" />
+                        :
+                        (
+                            props.success ? 
+                            <CheckIcon className="successCheck" />
+                            :
+                            'Upload Files!'
+                        )
+                    }
+                </button>
+            </Grid>
         </Grid>
     )
 }
