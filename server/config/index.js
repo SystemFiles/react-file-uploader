93ca389b77544345b061719c7cc7953e121d3ef5
Ben Sykes <ben@sykesdev.ca>
2020-11-29 11:20:11 -0500

    added missing explaination for clean options

--- server/config/index.js
+++ server/config/index.js
@@ -9,6 +9,7 @@ module.exports = {
 	SHARE_ENABLED       : process.env.SHARE_ENABLED,
 	MAX_FILES           : process.env.MAX_FILES || 6,
 	MAX_SIZE_MB         : process.env.MAX_SIZE_MB || 500,
+	// Clean directory on interval (days)
 	CLEAN_ENABLED       : process.env.CLEAN_ENABLED || false,
 	CLEAN_DIR_INTERVAL  : process.env.CLEAN_DIR_INTERVAL || 7
 }
